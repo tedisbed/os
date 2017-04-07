077131f7b70a6795dd0b792af8195b9d669d5081
Tristan Mitchell <tmitche9@nd.edu>
2017-04-06 20:37:54 -0400

    Working on custom algorithm

--- project5/main.c
+++ project5/main.c
@@ -15,6 +15,8 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
+int *custom_queue;
+int nframes, npages;
 int frames_in_use = 0;
 const char *mode;
 char *physmem;
@@ -22,19 +24,35 @@ struct disk *disk;
 
 void page_fault_handler(struct page_table *pt, int page)
 {
-	int frame, bits;
+	int frame, bits, x;
 	page_table_get_entry(pt, page, &frame, &bits);
 
 	if (bits == 1) {
 		page_table_set_entry(pt, page, frame, PROT_READ|PROT_WRITE);
-	} else if (frames_in_use < page_table_get_nframes(pt)) {
+		int limit;
+		if (frames_in_use <= nframes) {
+			limit = frames_in_use;
+		} else {
+			limit = nframes;
+		}
+		int start_swapping = 0;
+		for (x = 0; x < limit - 1; x++) {
+			if (custom_queue[x] == frame) {
+				start_swapping = 1;
+			}
+			if (start_swapping == 1) {
+				custom_queue[x] = custom_queue[x+1];
+			}
+		}
+		custom_queue[limit-1] = frame;
+	} else if (frames_in_use < nframes) {
 		page_table_set_entry(pt, page, frames_in_use, PROT_READ);
 		disk_read(disk, page, &physmem[frames_in_use * PAGE_SIZE]);
+		custom_queue[frames_in_use] = frame;
 		frames_in_use++;
 	} else if (!strcmp(mode, "rand")) {
-		int rand_frame = rand() % page_table_get_nframes(pt);
-		int x;
-		for (x = 0; x < page_table_get_npages(pt); x++) {
+		int rand_frame = rand() % nframes;
+		for (x = 0; x < npages; x++) {
 			page_table_get_entry(pt, x, &frame, &bits);
 			if (frame == rand_frame && bits != 0) {
 				if (bits == 3) {
@@ -47,10 +65,9 @@ void page_fault_handler(struct page_table *pt, int page)
 			}
 		}
 	} else if (!strcmp(mode, "fifo")) {
-		int first_frame = frames_in_use % page_table_get_nframes(pt);
+		int first_frame = frames_in_use % nframes;
 		frames_in_use++;
-		int x;
-		for (x = 0; x < page_table_get_npages(pt); x++) {
+		for (x = 0; x < npages; x++) {
 			page_table_get_entry(pt, x, &frame, &bits);
 			if (frame == first_frame && bits != 0) {
 				if (bits == 3) {
@@ -63,7 +80,23 @@ void page_fault_handler(struct page_table *pt, int page)
 			}
 		}
 	} else if (!strcmp(mode, "custom")) {
-		
+		int top_frame = custom_queue[0];
+		for (x = 0; x < npages; x++) {
+			page_table_get_entry(pt, x, &frame, &bits);
+			if (frame == top_frame && bits != 0) {
+				if (bits == 3) {
+					disk_write(disk, x, &physmem[frame * PAGE_SIZE]);
+				}
+				disk_read(disk, page, &physmem[frame * PAGE_SIZE]);
+				page_table_set_entry(pt, page, frame, PROT_READ);
+				page_table_set_entry(pt, x, 0, 0);
+			}
+		}
+
+		for (x = 0; x < nframes - 1; x++) {
+			custom_queue[x] = custom_queue[x+1];
+		}
+		custom_queue[nframes - 1] = top_frame;
 	}
 
 	printf("page fault on page #%d\n", page);
@@ -77,14 +110,16 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
+	npages = atoi(argv[1]);
+	nframes = atoi(argv[2]);
 	mode = argv[3];
 	if(strcmp(mode, "rand") && strcmp(mode, "fifo") && strcmp(mode, "custom")) {
 		fprintf(stderr, "unknown algorithm mode: %s\n", argv[3]);
 		return 1;
 	}
 	const char *program = argv[4];
+
+	custom_queue = malloc(nframes * sizeof(int));
 
 	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
@@ -118,6 +153,7 @@ int main( int argc, char *argv[] )
 
 	page_table_delete(pt);
 	disk_close(disk);
+	free(custom_queue);
 
 	return 0;
 }
